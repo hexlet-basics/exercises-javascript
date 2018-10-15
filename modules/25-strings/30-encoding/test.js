20717ecad2211aa8d195072b3e74809c4ba9dc79
Kirill Mokevnin <mokevnin@gmail.com>
2018-10-15 13:43:42 +0300

    fix tests

--- modules/25-strings/30-encoding/test.js
+++ modules/25-strings/30-encoding/test.js
@@ -2,7 +2,7 @@ const childProcess = require('child_process');
 const assert = require('assert');
 
 const stdout = childProcess.execSync(`node ${__dirname}/index.js`);
-const content = stdout.toString().trim();
+const content = stdout.toString().trim().replace(/\n/g, '');
 console.log(content);
 const expected = '~^%';
 assert.strictEqual(content, expected);
