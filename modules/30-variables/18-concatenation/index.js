dd0a4091e59e9170d75ea4503c0d054a4d43f073
Ulugbek Tuychiev <t.ulugbek.9@gmail.com>
2018-11-30 10:12:47 +0500

    fix concatenation test

--- modules/30-variables/18-concatenation/index.js
+++ modules/30-variables/18-concatenation/index.js
@@ -1,5 +1,5 @@
 const info = "We couldn't verify you mother's maiden name.";
-const intro = '\nHere is important information about your account security.';
+const intro = 'Here is important information about your account security.';
 
 const firstName = 'Joffrey';
 const greeting = 'Hello';
