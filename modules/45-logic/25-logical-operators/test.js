9e0c48b6ac2faa1e08ebeae3a59c1b8eb1e8785e
greybutton <greybutton@users.noreply.github.com>
2018-12-23 00:45:34 +0800

    add lesson logical operators (#16)

---
+++ modules/45-logic/25-logical-operators/test.js
@@ -0,0 +1,8 @@
+const assert = require('power-assert');
+const isLannisterSoldier = require(__dirname);
+
+assert(isLannisterSoldier('blue', null) === false);
+assert(isLannisterSoldier('red', 'man') === false);
+assert(isLannisterSoldier('red', 'lion') === true);
+assert(isLannisterSoldier('blue', 'lion') === true);
+assert(isLannisterSoldier('red', null) === true);
