943530452643f7004a027163203d3e9a0dcfad3d
Ulugbek Tuychiev <t.ulugbek.9@gmail.com>
2018-12-30 17:02:20 +0500

    fix missin output in variables

--- modules/30-variables/25-interpolation/test.js
+++ modules/30-variables/25-interpolation/test.js
@@ -3,5 +3,6 @@ const assert = require('assert');
 
 const stdout = childProcess.execSync(`node ${__dirname}/index.js`);
 const content = stdout.toString().trim();
+console.log(content);
 const expected = 'Do you want to eat, Arya?';
 assert.strictEqual(content, expected);
--- modules/30-variables/30-symbols/test.js
+++ modules/30-variables/30-symbols/test.js
@@ -3,5 +3,6 @@ const assert = require('assert');
 
 const stdout = childProcess.execSync(`node ${__dirname}/index.js`);
 const content = stdout.toString().trim();
+console.log(content);
 const expected = 'hodor';
 assert.strictEqual(content, expected);
