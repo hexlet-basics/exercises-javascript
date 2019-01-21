19ae07a6514c3f69e7f07ed40fcd70d18e0750a8
Kirill Mokevnin <mokevnin@gmail.com>
2019-01-21 10:29:52 +0400

    fix logic/if-else

--- modules/45-logic/40-if-else/test.js
+++ modules/45-logic/40-if-else/test.js
@@ -2,5 +2,5 @@ const { test, expect } = require('tests');
 
 test((f) => {
   expect(f('yandex.ru')).toBe('https://yandex.ru');
-  expect(f('https://yandex.ru')).toBe('https://yandex.ru');
+  expect(f('http://yandex.ru')).toBe('https://yandex.ru');
 });
