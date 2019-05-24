4ad0e93fbad291372f99fc52c6b0cb4d8fe35899
Sergey Karpuk <sergeykarpuk@yandex.by>
2019-05-24 22:54:41 +0300

    fix solution 38-properties/200-methods

--- modules/38-properties/200-methods/index.js
+++ modules/38-properties/200-methods/index.js
@@ -1,6 +1,6 @@
 const text = 'a MIND needs Books as a Sword needS a WHETSTONE, if it is to keep its edge.';
 
 // BEGIN
-const lowerText = text.toLowerCase(text);
+const lowerText = text.toLowerCase();
 console.log(lowerText);
 // END
