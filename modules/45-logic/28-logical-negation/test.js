b59669b5c99f7dbb95a9459cf31ba1c8d09b6706
Sergey Karpuk <sergeykarpuk@yandex.by>
2019-09-17 07:31:24 +0300

    fix test 45-logic/28-logical-negation

--- modules/45-logic/28-logical-negation/test.js
+++ modules/45-logic/28-logical-negation/test.js
@@ -2,8 +2,8 @@ const { test, expect } = require('tests');
 
 test((f) => {
   expect(f('blue', null)).toBe(true);
+  expect(f('red', null)).toBe(false);
   expect(f('red', 'man')).toBe(true);
   expect(f('red', 'lion')).toBe(false);
   expect(f('blue', 'lion')).toBe(false);
-  expect(f('blue', null)).toBe(true);
 });
