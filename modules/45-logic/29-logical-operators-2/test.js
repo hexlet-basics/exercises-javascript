f867988d76d980af07b157f41afed3b4b61cfef3
Sergey Karpuk <sergeykarpuk@yandex.by>
2020-01-14 01:30:58 +0300

    add test 45-logic/29-logical-operators-2

--- modules/45-logic/29-logical-operators-2/test.js
+++ modules/45-logic/29-logical-operators-2/test.js
@@ -4,5 +4,6 @@ test((f) => {
   expect(f('yellow', 'black')).toBe(true);
   expect(f('red', 'yellow')).toBe(false);
   expect(f('red', 'red')).toBe(false);
+  expect(f('red', 'black')).toBe(false);
   expect(f('black', 'black')).toBe(true);
 });
