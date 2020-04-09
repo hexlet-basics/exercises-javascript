d2141b974aa16917626939afe8626e350a347027
Colonizator1 <39800054+Colonizator1@users.noreply.github.com>
2020-04-09 11:48:04 +0300

    add test with card as number

--- modules/40-define-functions/350-naming/test.js
+++ modules/40-define-functions/350-naming/test.js
@@ -6,4 +6,8 @@ test((f) => {
   expect(actual1).toEqual('****5678');
   const actual2 = f(card1, 2);
   expect(actual2).toEqual('**5678');
+  
+  const card2 = 1234567812345678;
+  const actual3 = f(card2);
+  expect(actual2).toEqual('****5678');
 });
