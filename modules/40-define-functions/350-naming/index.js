ea1bc98803f1ec2562ecc5a38bd3e8c899ab0846
Kirill Mokevnin <mokevnin@gmail.com>
2020-04-09 09:10:32 -0400

    fix define-functions/naming

--- modules/40-define-functions/350-naming/index.js
+++ modules/40-define-functions/350-naming/index.js
@@ -1,6 +1,6 @@
 // BEGIN
 const getHiddenCard = (cardNumber, starsCount = 4) => {
-  const last4Digits = cardNumber.toString().slice(-4);
+  const last4Digits = cardNumber.slice(-4);
   return last4Digits.padStart(starsCount + last4Digits.length, '*');
 };
 // END
