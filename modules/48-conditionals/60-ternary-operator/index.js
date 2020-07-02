9864148c4d94c0dd964a6ec8ee9acda91fe75282
Sergey Karpuk <sergeykarpuk@yandex.by>
2020-07-02 17:18:25 +0300

    refactor(48-conditionals/60-ternary-operator): improve solution

--- modules/48-conditionals/60-ternary-operator/index.js
+++ modules/48-conditionals/60-ternary-operator/index.js
@@ -2,7 +2,7 @@ import { reverse } from 'hexlet-basics/string';
 
 // BEGIN
 const convertText = (text) => {
-  const reversable = text.length > 0 && text[0] !== text[0].toUpperCase();
+  const reversable = text[0] !== text[0].toUpperCase();
   return reversable ? reverse(text) : text;
 };
 // END
