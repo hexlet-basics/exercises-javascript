1e9610ebeaab817c245552a2e892ad0398cb0c83
Roman Ashikov <rmn.shkv@gmail.com>
2020-09-11 13:37:35 +0700

    (48/60) fix solution / add test

--- modules/48-conditionals/60-ternary-operator/index.js
+++ modules/48-conditionals/60-ternary-operator/index.js
@@ -2,6 +2,10 @@ import { reverse } from 'hexlet-basics/string';
 
 // BEGIN
 const convertText = (text) => {
+  if (text === '') {
+    return '';
+  }
+
   const reversable = text[0] !== text[0].toUpperCase();
   return reversable ? reverse(text) : text;
 };
