8657ff2abf6922a5cdc19315de8fd851b0cb2436
Maksim Litvinov <litvinovmksm@gmail.com>
2021-01-15 18:21:14 +0300

    improve test

--- modules/40-define-functions/340-define-functions-default-arguments/test.js
+++ modules/40-define-functions/340-define-functions-default-arguments/test.js
@@ -2,5 +2,7 @@ import { test, expect } from 'hexlet-basics/tests';
 
 test((f) => {
   const expected1 = 'Tywin Lannister';
+  const expected2 = 'Rhaella Targaryen';
   expect(f('Cersei Lannister')).toEqual(expected1);
+  expect(f('Daenerys Targaryen', 'mother')).toEqual(expected2);
 });
