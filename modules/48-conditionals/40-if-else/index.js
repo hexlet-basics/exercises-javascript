11c29d09adf62198a39896991f4865abe1ceede9
Sergey Karpuk <sergeykarpuk@yandex.by>
2022-02-17 11:46:57 +0300

    revert

--- modules/48-conditionals/40-if-else/index.js
+++ modules/48-conditionals/40-if-else/index.js
@@ -1,19 +1,12 @@
 // BEGIN
-// const buildUrl = (url, domain) => {
-//   let normalizedUrl;
-//   if (url.startsWith('/')) {
-//     normalizedUrl = url.slice(1);
-//   } else {
-//     normalizedUrl = url;
-//   }
-//   return `https://${domain}/${normalizedUrl}`;
-// };
-
-const buildUrl = (path, domain) =>{
-  if (path.startsWith("/")){
-    return `https://${domain}${path}`;
+const buildUrl = (url, domain) => {
+  let normalizedUrl;
+  if (url.startsWith('/')) {
+    normalizedUrl = url.slice(1);
+  } else {
+    normalizedUrl = url;
   }
-  return `https://${domain}/${path}`;
+  return `https://${domain}/${normalizedUrl}`;
 };
 // END
 
