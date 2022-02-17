fe8e5efe960d524d9f5b0c4efdeeaa9179eda2c8
Sergey Karpuk <sergeykarpuk@yandex.by>
2022-02-17 11:28:01 +0300

    fix typo

--- modules/48-conditionals/40-if-else/index.js
+++ modules/48-conditionals/40-if-else/index.js
@@ -1,12 +1,19 @@
 // BEGIN
-const buildUrl = (url, domain) => {
-  let normalizedUrl;
-  if (url.startsWith('/')) {
-    normalizedUrl = url.slice(1);
-  } else {
-    normalizedUrl = url;
+// const buildUrl = (url, domain) => {
+//   let normalizedUrl;
+//   if (url.startsWith('/')) {
+//     normalizedUrl = url.slice(1);
+//   } else {
+//     normalizedUrl = url;
+//   }
+//   return `https://${domain}/${normalizedUrl}`;
+// };
+
+const buildUrl = (path, domain) =>{
+  if (path.startsWith("/")){
+    return `https://${domain}${path}`;
   }
-  return `https://${domain}/${normalizedUrl}`;
+  return `https://${domain}/${path}`;
 };
 // END
 
