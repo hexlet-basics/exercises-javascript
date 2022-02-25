a065e1770b69e250eb50ac9116cec1360eb0e79e
Maksim Litvinov <maksim.litvinov@hexlet.io>
2022-02-25 14:19:07 +0300

    fix test

--- modules/35-calling-functions/270-deterministic/test.js
+++ modules/35-calling-functions/270-deterministic/test.js
@@ -1,9 +1,10 @@
 // @ts-check
-
 import { expectOutput, expect } from 'hexlet-basics/tests';
 
 expectOutput((content) => {
+  expect(content).not.toEqual('');
   const value = Number(content);
+  expect(Number.isInteger(value)).toEqual(true);
   expect(value).toBeGreaterThanOrEqual(0);
   expect(value).toBeLessThanOrEqual(10);
 });
