cc16464b8b15ef0c932fb89832749df18723a53c
Maksim Litvinov <litvinovmksm@gmail.com>
2022-02-25 14:58:44 +0300

    Fix 25 (#545)
    * fix test
    
    * fix test
    
    * fix test
    
    Co-authored-by: Maksim Litvinov <maksim.litvinov@hexlet.io>

--- modules/40-define-functions/150-define-functions-return/test.js
+++ modules/40-define-functions/150-define-functions-return/test.js
@@ -4,6 +4,6 @@ import { test, expect } from '@jest/globals';
 import f from './index.js';
 
 test('test', () => {
-  expect(f()).toBeGreaterThan(0);
-  expect(f()).toBeLessThan(10);
+  expect(f()).toBeGreaterThanOrEqual(0);
+  expect(f()).toBeLessThanOrEqual(10);
 });
