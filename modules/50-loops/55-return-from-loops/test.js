e7ba84a828a57cb5542aba2f019727c6107f359c
Roman Ashikov <rmn.shkv@gmail.com>
2022-03-08 14:56:09 +0700

    (50-loops/55-return-from-loops) remove game of thrones

--- modules/50-loops/55-return-from-loops/test.js
+++ modules/50-loops/55-return-from-loops/test.js
@@ -2,9 +2,9 @@ import { test, expect } from '@jest/globals';
 import f from './index.js';
 
 test('test', () => {
-  expect(f('Renly', 'R')).toBe(true);
-  expect(f('Renly', 'r')).toBe(false);
-  expect(f('Tommy', 'm')).toBe(true);
-  expect(f('Tommy', 'd')).toBe(false);
-  expect(f('Tommy', 'y')).toBe(true);
+  expect(f('Hexlet', 'H')).toBe(true);
+  expect(f('Hexlet', 'h')).toBe(false);
+  expect(f('Awesomeness', 'm')).toBe(true);
+  expect(f('Awesomeness', 'd')).toBe(false);
+  expect(f('Awesomeness', 'o')).toBe(true);
 });
