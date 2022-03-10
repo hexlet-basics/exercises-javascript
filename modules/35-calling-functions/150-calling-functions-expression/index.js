f551553ba511c648749d241435faaa9cdbe9f608
Maksim Litvinov <maksim.litvinov@hexlet.io>
2022-03-10 15:29:01 +0300

    add function length

--- modules/35-calling-functions/150-calling-functions-expression/index.js
+++ modules/35-calling-functions/150-calling-functions-expression/index.js
@@ -1,6 +1,8 @@
+import { length } from 'hexlet-basics/string';
+
 const text = 'Never forget what you are, for surely the world will not';
 
 // BEGIN
-const result = `First: ${text[0]}\nLast: ${text[text.length - 1]}`;
+const result = `First: ${text[0]}\nLast: ${text[length(text) - 1]}`;
 console.log(result);
 // END
