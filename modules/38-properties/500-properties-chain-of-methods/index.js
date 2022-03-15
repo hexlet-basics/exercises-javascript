78c42a1e449ee1a1551d7ce76390749d106db4a1
Sergey Karpuk <sergeykarpuk@yandex.by>
2022-03-15 21:23:45 +0300

    replace substring() with slica()e method

--- modules/38-properties/500-properties-chain-of-methods/index.js
+++ modules/38-properties/500-properties-chain-of-methods/index.js
@@ -3,5 +3,5 @@
 const text = 'When \t\n you play a \t\n game of thrones you win or you die.';
 
 // BEGIN
-console.log(text.substring(4, 15).trim().length);
+console.log(text.slice(4, 15).trim().length);
 // END
