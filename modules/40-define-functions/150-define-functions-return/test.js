07d5af520f97464a8825271b9cc23b85a14acad0
Maksim Litvinov <litvinovmksm@gmail.com>
2022-03-22 14:11:48 +0300

    fix exercises (#574)
    Co-authored-by: Maksim Litvinov <maksim.litvinov@hexlet.io>

--- modules/40-define-functions/150-define-functions-return/test.js
+++ modules/40-define-functions/150-define-functions-return/test.js
@@ -4,6 +4,7 @@ import { test, expect } from '@jest/globals';
 import f from './index.js';
 
 test('test', () => {
+  expect(Number.isInteger(f())).toEqual(true);
   expect(f()).toBeGreaterThanOrEqual(0);
   expect(f()).toBeLessThanOrEqual(10);
 });
