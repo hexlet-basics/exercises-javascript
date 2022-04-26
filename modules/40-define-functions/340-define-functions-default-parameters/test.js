43ef6ff0924846e09c3a228b021bd9d5edfa35a0
ssssank <ssssank@gmail.com>
2022-04-26 16:59:18 +0300

    add tests

--- modules/40-define-functions/340-define-functions-default-parameters/test.js
+++ modules/40-define-functions/340-define-functions-default-parameters/test.js
@@ -6,4 +6,6 @@ import f from './index.js';
 test('test', () => {
   expect(f('1234123412341234')).toEqual('****1234');
   expect(f('1234123412344321')).toEqual('****4321');
+  expect(f('1234123412344321', 2)).toEqual('**4321');
+  expect(f('1234123412341234', 12)).toEqual('************1234');
 });
