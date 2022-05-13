702069a018fb35f97eb9ad2cc79aea7ddf86e083
Sergey Karpuk <sergeykarpuk@yandex.by>
2022-05-13 11:11:30 +0300

    fix(50-loops/30-syntax-sugar): improve testfg

--- modules/50-loops/30-syntax-sugar/test.js
+++ modules/50-loops/30-syntax-sugar/test.js
@@ -4,5 +4,5 @@ import f from './index.js';
 test('test', () => {
   const text = 'If I look back I am lost';
   expect(f(text, 'I')).toEqual('f  look back  am lost');
-  expect(f('zz zorro', 'z')).toEqual(' orro');
+  expect(f('zz Zorro', 'z')).toEqual(' Zorro');
 });
