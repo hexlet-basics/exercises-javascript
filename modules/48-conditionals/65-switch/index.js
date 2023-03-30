7d8ecb362a7f2dc3a1494919160cb8660fa65901
emp7yhead <artyomkropp@gmail.com>
2023-03-30 16:08:19 +0900

    update exercise

--- modules/48-conditionals/65-switch/index.js
+++ modules/48-conditionals/65-switch/index.js
@@ -8,7 +8,7 @@ const getNumberExplanation = (number) => {
     case 42:
       return 'answer for everything';
     default:
-      return null;
+      return 'just a number';
   }
 };
 // END
--- modules/48-conditionals/65-switch/test.js
+++ modules/48-conditionals/65-switch/test.js
@@ -2,7 +2,7 @@ import { test, expect } from '@jest/globals';
 import f from './index.js';
 
 test('test', () => {
-  expect(f(0)).toBeNull();
+  expect(f(0)).toBe('just a number');
   expect(f(666)).toBe('devil number');
   expect(f(42)).toBe('answer for everything');
   expect(f(7)).toBe('prime number');
