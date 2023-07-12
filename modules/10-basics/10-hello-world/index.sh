a70f089f235e610919c8ebe007bb79fbbfeebfbf
usernaimandrey <anshlyapnikov@yandex.ru>
2023-07-12 11:03:58 +0300

    Revert "add content"
    This reverts commit f303fa99fc853bf143bff01a2de1655a48ad10b1.

--- modules/10-basics/10-hello-world/index.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/env bash
-
-# BEGIN
-echo 'Hello, World!'
-# END
--- modules/10-basics/10-hello-world/test_index.sh
+++
@@ -1,9 +0,0 @@
-test_hello_world() {
-  echo
-  echo
-  . index.sh || exit 1
-  result=`. index.sh`
-  assert_equals "Hello, World!" "$result"
-  echo
-  echo
-}
